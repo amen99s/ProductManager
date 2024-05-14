3441ecda0a35fe363cd69b0c2dd7934a1466da49
amen99s <amensahed9@gmail.com>
2024-05-13 21:07:46 -0400

    Initial commit

---
+++ AminRasoulFinal/AddNewProduct.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AminRasoulFinal
+{
+    /// <summary>
+    /// Interaction logic for AddNewProduct.xaml
+    /// </summary>
+    public partial class AddNewProduct : Window
+    {
+        public AddNewProduct()
+        {
+            InitializeComponent();
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            using (var context = new MYLocalDBEntities())
+            {
+
+                var categories = context.Categories.ToList();
+
+
+                combobox_category.ItemsSource = categories;
+                combobox_category.DisplayMemberPath = "CategoryName";
+                combobox_category.SelectedValuePath = "CategoryID";
+            }
+        }
+
+        private void txtbox_productName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtbox_price_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void combobox_category_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddProduct(string productName, decimal price, int categoryId)
+        {
+            using (var context = new MYLocalDBEntities())
+            {
+                var newProduct = new Product
+                {
+                    ProductName = productName,
+                    UnitPrice = price,
+                    CategoryID = categoryId
+                };
+
+                context.Products.Add(newProduct);
+                context.SaveChanges();
+                MessageBox.Show("Product added successfully!");
+            }
+        }
+            private void btn_addProduct_Click(object sender, RoutedEventArgs e)
+            {
+                string product_name = txtbox_productName.Text.Trim();
+                string price = txtbox_price.Text.Trim();
+                var selectedCategoryId = combobox_category.SelectedValue;
+
+                if (string.IsNullOrEmpty(product_name) || product_name.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Please enter a valid product name (it should not contain numbers).");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(price) || !decimal.TryParse(price, out decimal priceNum))
+                {
+                    MessageBox.Show("Please enter a valid price (it should be a numeric value).");
+                    return;
+                }
+                if (selectedCategoryId == null)
+                {
+                    MessageBox.Show("Please select a category.");
+                    return;
+                }
+                AddProduct(product_name, priceNum, (int)selectedCategoryId);
+            }
+
+
+
+        }
+    } 
+
---
+++ AminRasoulFinal/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AminRasoulFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void LoadCategories()
+        {
+            using (var context = new MYLocalDBEntities()) 
+            {
+                
+                var categories = context.Categories.ToList();
+
+                
+                combobox_getproductByCategory.ItemsSource = categories;
+                combobox_getproductByCategory.DisplayMemberPath = "CategoryName"; 
+                combobox_getproductByCategory.SelectedValuePath = "CategoryID"; 
+            }
+        }
+
+        private void combobox_getproductByCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+            if (combobox_getproductByCategory.SelectedValue != null)
+            {
+                int selectedCategoryId = (int)combobox_getproductByCategory.SelectedValue;
+                using (var context = new MYLocalDBEntities())
+                {
+                    var products = context.Products.Where(p => p.CategoryID == selectedCategoryId).ToList();
+                    grid_data.ItemsSource = products;
+                }
+            }
+
+        }
+
+
+        private void btn_search_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = txtBox_search.Text.Trim();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                using (var context = new MYLocalDBEntities()) 
+                {
+                    var products = context.Products.Where(p => p.ProductName.Contains(searchTerm)).ToList();
+                    if (products.Count > 0)
+                    {
+                        grid_data.ItemsSource = null;
+                        grid_data.ItemsSource = products;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No products found with the given search term.");
+                        grid_data.ItemsSource = null;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a search term.");
+            }
+        }
+
+        private void btn_addNewProdut_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewProduct addnewproduct = new AddNewProduct();
+            addnewproduct.ShowDialog();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        //this btn of get All product
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            LoadCategories();
+
+            if(combobox_getproductByCategory.SelectedValue == null)
+            {
+                using (var context = new MYLocalDBEntities())
+                {
+                    var products = context.Products.ToList();
+                    grid_data.ItemsSource = products;
+                }
+            }
+        }
+
+        //btn of Clear Data
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(grid_data.ItemsSource != null)
+            {
+                grid_data.ItemsSource = null;
+            }
+            else
+            {
+                MessageBox.Show("Its already empty");
+            }
+            
+        }
+
+        private void txtBox_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
